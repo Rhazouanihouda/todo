ece1c495022d9486110be40f7e51e1efb1346180
Rhazouanihouda <rhazouanih98@gmail.com>
2022-06-14 21:22:00 -0400

    uopdate

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,7 +3,7 @@
     <v-row class="text-center">
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          Todo list
+          Todo listhouda
         </h1>
 
       </v-col>
